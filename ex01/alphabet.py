6c4cbe006de659936812fbe6c93c76c03313cab4
akae <c0a21020cb@edu.teu.ac.jp>
2022-06-14 17:08:23 +0900

    ahaha

---
+++ ex01/alphabet.py
@@ -0,0 +1,34 @@
+import random
+
+Kurikaeshi = 5
+num_taisho = 10
+num_kesson = 2
+
+def main():
+    for i in range(Kurikaeshi):
+        kaito = mondai()
+        kaitou(kaito)
+
+def mondai():
+    alp = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    mondai = random.sample(alp,10)
+    
+    print("対象文字：")
+    print(mondai)
+    print("表示文字：")
+    hyouji = random.sample(mondai,8)
+    print(hyouji)
+    
+    return mondai
+
+def kaitou(seikai):
+    urans = int(input("欠損文字はいくつあるでしょうか？："))
+    if urans == num_kesson:
+        print("correct")
+    else:
+        print("wrong")
+
+if __name__ == "__main__":
+    main()
+
+
