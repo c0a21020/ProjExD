8ebfd6aa043e0cbfd59bb30d37cdc7ebde42babc
akae <c0a21020cb@edu.teu.ac.jp>
2022-06-21 15:28:01 +0900

    enshulast

--- ex02/calc1.py
+++ ex02/calc1.py
@@ -4,27 +4,38 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("", f"{num}のボタンがクリックされました")
+    #tkm.showinfo("", f"{num}のボタンがクリックされました")
+    entry.insert(tk.END, num)
+
+def click_e(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, str(res))
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title("tk")
     root.geometry("300x500")
-    root.title("電卓")
+    entry = tk.Entry(root, justify="right", width=12, font=("Times New Roman", 30))
+    entry.grid(row=0, column=1, columnspan=4)
 
-    r,c = 0,0
-    for num in range(9,-1,-1):
-        button = tk.Button(root,text = num ,width = 4,height = 2,font = ("Times New Roman",20))
+    r = 1
+    c = 1
+    for i, num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"], 1):
+        button = tk.Button(root, text=str(num), font=("Times New Roman", 30))
+        button.grid(row=r, column=c, padx=10, pady=10)
+        button.bind("<1>", button_click)
+        if i%3 == 0:
+            r+=1
+            c=0
+        c+=1
 
-        button.bind("<1>",button_click)
-
-        button.grid(row=r,column=c)
-        c += 1
-        if (num-1)%3 == 0:
-            r += 1
-            c = 0
-
+    btn = tk.Button(root,
+                text="=",
+                font=("Times New Roman", 30)
+                    )   
+    btn.bind("<1>", click_e)
+    btn.grid(row=r, column=c, padx=10, pady=10)
 
     root.mainloop()
-
-
-
