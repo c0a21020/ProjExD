11b1e7d26b593d62035f2a938dcff2c5bc11e459
akae <c0a21020cb@edu.teu.ac.jp>
2022-06-21 14:35:55 +0900

    enshu3

---
+++ ex02/calc1.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("", f"{num}のボタンがクリックされました")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("300x500")
+    root.title("電卓")
+
+    r,c = 0,0
+    for num in range(9,-1,-1):
+        button = tk.Button(root,text = num ,width = 4,height = 2,font = ("Times New Roman",30))
+
+        button.bind("<1>",button_click)
+
+        button.grid(row=r,column=c)
+        c += 1
+        if (num-1)%3 == 0:
+            r += 1
+            c = 0
+
+
+    root.mainloop()
+
+
+
