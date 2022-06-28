168f6702b969a27aaca9ffba9c8b2b558fe3102c
akae <c0a21020cb@edu.teu.ac.jp>
2022-06-28 14:35:29 +0900

    q5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f"{key}osareta")
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("mayoerukoukaton")
@@ -9,4 +14,7 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/8.png")
     cx,cy = 300,400
     canvas.create_image(cx, cy, image=tori, tag="tori")
+    
+    root.bind("<KeyPress>", key_down)
+    
     root.mainloop()
