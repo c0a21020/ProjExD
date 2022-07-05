eb676994f9ca168c34774c114286f7e43266a9f4
akae <c0a21020cb@edu.teu.ac.jp>
2022-07-05 14:05:37 +0900

    renshu1-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,10 +4,17 @@ import sys
 def main():
     clock = pg.time.Clock()
     
-    pg.display.set_caption("逃げろ")
-    screen = pg.display.set_mode((1600,900))
 
-    clock.tick(0.5)
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen_sfc = pg.display.set_mode((1600,900))
+    screen_rct = screen_sfc.get_rect()
+    bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bgimg_rct = bgimg_sfc.get.rect()
+    bgimg_sfc.blit(bgimg_sfc,bgimg_rct)
+    
+    pg.display.update()
+
+    clock.tick(0.2)
 
 if __name__ == "__main__":
     pg.init()
