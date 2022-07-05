03eb9161978035f8434a479421d383faa999558b
akae <c0a21020cb@edu.teu.ac.jp>
2022-07-05 14:32:42 +0900

    renshu4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,12 +11,32 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()
     bgimg_sfc.blit(bgimg_sfc,bgimg_rct)
 
+    koukaton_sfc = pg.image.load("fig/9.png")
+    koukaton_sfc = pg.transform.rotozoom(koukaton_sfc,0,2.0)
+    koukaton_rct = koukaton_sfc.get_rect()
+    koukaton_rct.center = 900,400
+    
+
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP] == True:    
+            koukaton_rct.centery -= 1      #y...-1
+        if key_states[pg.K_DOWN] == True:
+            koukaton_rct.centery += 1      #y...+1
+        if key_states[pg.K_LEFT] == True:
+            koukaton_rct.centerx -= 1      #x...-1
+        if key_states[pg.K_RIGHT] == True: 
+            koukaton_rct.centerx += 1      #x...+1
+
+        screen_sfc.blit(koukaton_sfc,koukaton_rct)
+
         pg.display.update()
         clock.tick(1000)
 
