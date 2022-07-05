a6a539ceaafb86d507dfc0696871a85ddf837299
akae <c0a21020cb@edu.teu.ac.jp>
2022-07-05 14:15:08 +0900

    renshu2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,20 +4,25 @@ import sys
 def main():
     clock = pg.time.Clock()
     
-
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600,900))
     screen_rct = screen_sfc.get_rect()
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
-    bgimg_rct = bgimg_sfc.get.rect()
+    bgimg_rct = bgimg_sfc.get_rect()
     bgimg_sfc.blit(bgimg_sfc,bgimg_rct)
-    
-    pg.display.update()
 
-    clock.tick(0.2)
+    while True:
+        screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        
+        pg.display.update()
+        clock.tick(1000)
+
 
 if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exist()
+    sys.exit()
