620075b671c48ed767b7641be0cac1e21308b957
akae <c0a21020cb@edu.teu.ac.jp>
2022-07-19 17:37:17 +0900

    コメントアウト書き、コード規約に基づいて

--- ex06/Maze.py
+++ ex06/Maze.py
@@ -1,29 +1,28 @@
 import random
-size = 50
+size = 50     #１マスの幅
 
 def make_maze():
-    yoko = 27
+    yoko = 27         #27×15のマス
     tate = 15
     XP = [ 0, 1, 0, -1]
     YP = [-1, 0, 1,  0]
-
     maze_lst = []
-    #1
+    
+    #1 中身の数字が全部０の多次元リストを生成
     for y in range(tate):
         maze_lst.append([0]*yoko)
-    #2
+    #2　　（壁を作る）マスの最初と最後の行にあたるリストの部分を０→１にする
     for x in range(yoko):
         maze_lst[0][x] = 1
         maze_lst[tate-1][x] = 1
-    #3
     for y in range(1, tate-1):
         maze_lst[y][0] = 1
         maze_lst[y][yoko-1] = 1
-    #4
+    #３　　（壁を作る）マスの最初と最後の列にあたるリストの部分を０→１にする
     for y in range(2, tate-2, 2):
         for x in range(2, yoko-2, 2):
             maze_lst[y][x] = 1
-    #5
+    #４　　（迷路を作る）壁の内側にランダムで迷路の道をつくる
     for y in range(2, tate-2, 2):
         for x in range(2, yoko-2, 2):
             if x > 2: rnd = random.randint(0, 2)
@@ -32,13 +31,16 @@ def make_maze():
 
     return maze_lst
 
+
 def show_maze(canvas, maze_lst):
-    color = ["white", "skyblue","pink"]
+    color = ["white", "skyblue","pink"] #マスを壁、道、スタート・ゴールで色分けするためのリスト
     start = 1
     goal_x, goal_y = 26, 14
+
     for y in range(len(maze_lst)):
         for x in range(len(maze_lst[y])):
             canvas.create_rectangle(x*size, y*size, x*size+size, y*size+size, 
-                                    fill=color[maze_lst[y][x]], outline=color[maze_lst[y][x]])
-    canvas.create_rectangle(size, size, start*size+size, start*size+size, fill=color[2], outline=color[2])
-    canvas.create_rectangle((goal_x-1)*size, (goal_y-1)*size, goal_x*size, goal_y*size, fill=color[2], outline=color[2])
+                                    fill=color[maze_lst[y][x]], outline=color[maze_lst[y][x]])                           #壁か道でないところであれば水色、迷路の道なら白
+    
+    canvas.create_rectangle(size, size, start*size+size, start*size+size, fill=color[2], outline=color[2])               #スタート地点をピンクに
+    canvas.create_rectangle((goal_x-1)*size, (goal_y-1)*size, goal_x*size, goal_y*size, fill=color[2], outline=color[2]) #ゴール地点をピンクに
--- ex06/Mazerun.py
+++ ex06/Mazerun.py
@@ -1,79 +1,82 @@
 import tkinter as tk
-from tracemalloc import start
 import Maze as mm
 from tkinter import messagebox
 import random
-#import datetime
+import time
 
 
-def key_down(event):
+def key_down(event): #keyが押されているときに行われる処理
     global key
     key = event.keysym
-    #print(f"{key},osareta")
 
-def key_up(event):
-    global key
+def key_up(event): #keyが何も押されていない時の処理
+    global key, begin
     key = ""
     
-    if cx == 1274 and cy == 674: # ゴール判定
-        messagebox.showinfo("GOAL", "ゴールしました。")  
+    if cx == 1274 and cy == 674: #こうかとんの座標がゴールの座標と一致した時、
+                                 #こうかとんがゴールしたと判定して、ここまでの経過時間をメッセージボックスでプレイヤーに表示する
+        end = time.time() - begin
+        messagebox.showinfo("GOAL", f"{end}秒かかりました。")  
 
-def main_proc(): #リアルタイム処理であるので引数にeventはいらない
-    global cx,cy,mx,my,size
-    delta = {"Up":[0,-1],
-            "Down":[0,+1],
-            "Left":[-1,0],
-            "Right":[+1,0],
-            "":[0,0]}#押されているキーkey、値は移動幅リスト「x、y」
+def main_proc():               #リアルタイム処理であるので引数にevent
+    global cx, cy, mx, my, size
+    delta = {"Up":[0, -1],      #押されているキーkey、値は移動幅リスト「x、y」
+            "Down":[0, +1],
+            "Left":[-1, 0],
+            "Right":[+1, 0],
+            "":[0, 0]}
     try:
-        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
-            my,mx = my+delta[key][1], mx+delta[key][0]
+        if maze_bg[my + delta[key][1]][mx + delta[key][0]] == 0:    #移動しようとした先が壁だった場合
+            my, mx = my + delta[key][1], mx+delta[key][0]
     except:
         pass
 
-    cx,cy = mx*size+24, my*size+24
-    canvas.coords("tori",cx,cy)
+    cx, cy = mx*size+24, my*size+24 #こうかとんの座標
+    canvas.coords("tori", cx, cy)
     
     root.after(100, main_proc)
 
-def enemys():
-    global mx,my,enemy_x,enemy_y
-    delta = {"Up":[0,-1],
-            "Down":[0,+1],
-            "Left":[-1,0],
-            "Right":[+1,0],
-            "":[0,0]}
+'''def enemys():                                #ランダムで迷路内を動く敵の関数
+    global mx, my, enemy_x, enemy_y, rdm_x, rdm_y
+    delta = {"Up":[0, -1],
+            "Down":[0, +1],
+            "Left":[-1, 0],
+            "Right":[+1, 0],
+            "":[0, 0]}
 
-    while cx != 1274 and cy != 674: #こうかとんがゴールするまで
-        enemy_x,enemy_y = mx*size+24, my*size+24
-        mx += random.randint(-1,1)
-        my += random.randint(-1,1)
-        canvas.coords("bird",enemy_x,enemy_y)
-        root.after(100, main_proc)
+    while cx != 1274 and cy != 674:          #こうかとんがゴールするまで次のことがやりたかった
+        print(0)                          #（←実行して関数が動いていることはこれで確認できたが、canvasが開かない...）
+        rdm_x = random.randint(-1, 1)        #１、実行されたら、迷路内のランダムな初期位置に敵が出現する
+        rdm_y = random.randint(-1, 1)        #２、ランダムに迷路内を動くように敵の位置を変えたかった。
+        enemy_x += 50 * rdm_x                 #while文のなかで書けば、ゴールするまで何回もenemy_x,enemy_y変わると思ってたけど、実は違う？
+        enemy_y += 50 * rdm_y
+        canvas.coords("bird", enemy_x, enemy_y)
+        
         try:
-            if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
-                my,mx = my+delta[key][1], mx+delta[key][0]
+            if maze_bg[my + delta[key][1]][mx + delta[key][0]] == 0:
+                my, mx = my + delta[key][1], mx + delta[key][0]
         except:
             pass
-        
     
+    root.after(100, main_proc)'''
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("空飛ぶこうかとん")
-    #begin = datetime.datetime.now()
+    root.title("個人で考えた迷路")
+    begin = time.time()
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="skyblue")   
     canvas.pack()
     maze_bg = mm.make_maze()
     mm.show_maze(canvas, maze_bg)
-    tori = tk.PhotoImage(file="fig/3.png")
-    enemy = tk.PhotoImage(file="fig/6.png")
-    cx,cy = 0,0
-    enemy_x, enemy_y = 0,0 
+    tori = tk.PhotoImage(file = "fig/3.png")
+    #敵キャラなので一度コメントアウト enemy = tk.PhotoImage(file="fig/6.png")
+    cx, cy = 0, 0
+    enemy_x, enemy_y,rdm_x,rdm_y = 0, 0, 0, 0 
     canvas.create_image(cx, cy, image=tori, tag="tori")
-    canvas.create_image(enemy_x,enemy_y, image=enemy, tag="enemy")
-    
+    #敵キャラなので一度コメントアウト canvas.create_image(enemy_x,enemy_y, image=enemy, tag="enemy")
+
     size = 50
     key = ""
     mx = 1
@@ -83,10 +86,7 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
     
     main_proc()
-    enemys()
     Start = tk.Label(text='START', foreground='black', background='#ffaacc')
     Start.place()
-    #end = datetime.datetime.now()
-    #print(f"所要時間：{(end-begin).seconds}秒かかりました")
-    print(cx,cy)
+    #敵キャラの関数なので一度コメントアウト　enemys()
     root.mainloop()
